06181590f453389e34d92ff61eff48ba1e71e7a8
bantheus <matheuschmidt.b@gmail.com>
2024-05-15 15:23:39 -0300

    ✨ feat: add transaction validation

--- src/main/java/com/bantheus/picpaysimplificado/transaction/TransactionService.java
+++ src/main/java/com/bantheus/picpaysimplificado/transaction/TransactionService.java
@@ -1,7 +1,9 @@
 package com.bantheus.picpaysimplificado.transaction;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bantheus.picpaysimplificado.exception.InvalidTransactionException;
 import com.bantheus.picpaysimplificado.wallet.Wallet;
 import com.bantheus.picpaysimplificado.wallet.WalletRepository;
 import com.bantheus.picpaysimplificado.wallet.WalletType;
@@ -16,11 +18,15 @@ public class TransactionService {
     this.walletRepository = walletRepository;
   }
 
+  @Transactional
   public Transaction create(Transaction transaction) {
+    // Validate transaction
     validate(transaction);
 
+    // Save transaction
     var newTransaction = transactionRepository.save(transaction);
 
+    // Credit payee wallet
     var wallet = walletRepository.findById(transaction.payer()).get();
     walletRepository.save(wallet.debit(transaction.value()));
 
@@ -31,8 +37,8 @@ public class TransactionService {
     walletRepository.findById(transaction.payee())
       .map(payee -> walletRepository.findById(transaction.payer())
         .map(payer -> isTransactionValid(transaction, payer) ? transaction : null)
-        .orElseThrow())
-      .orElseThrow();
+        .orElseThrow(() -> new InvalidTransactionException("Invalid transaction: %s".formatted(transaction))))
+      .orElseThrow(() -> new InvalidTransactionException("Invalid transaction: %s".formatted(transaction)));
   }
 
   private boolean isTransactionValid(Transaction transaction, Wallet payer) {
