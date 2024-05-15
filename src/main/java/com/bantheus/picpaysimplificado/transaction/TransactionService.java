811a069c4785da8b9e4ce2e29275594aa1fbc086
bantheus <matheuschmidt.b@gmail.com>
2024-05-15 14:22:57 -0300

    🚧 feat: add transaction validate

--- src/main/java/com/bantheus/picpaysimplificado/transaction/TransactionService.java
+++ src/main/java/com/bantheus/picpaysimplificado/transaction/TransactionService.java
@@ -2,7 +2,9 @@ package com.bantheus.picpaysimplificado.transaction;
 
 import org.springframework.stereotype.Service;
 
+import com.bantheus.picpaysimplificado.wallet.Wallet;
 import com.bantheus.picpaysimplificado.wallet.WalletRepository;
+import com.bantheus.picpaysimplificado.wallet.WalletType;
 
 @Service
 public class TransactionService {
@@ -15,11 +17,27 @@ public class TransactionService {
   }
 
   public Transaction create(Transaction transaction) {
+    validate(transaction);
+
     var newTransaction = transactionRepository.save(transaction);
 
     var wallet = walletRepository.findById(transaction.payer()).get();
     walletRepository.save(wallet.debit(transaction.value()));
 
     return newTransaction;
+  }
+
+  private void validate(Transaction transaction) {
+    walletRepository.findById(transaction.payee())
+      .map(payee -> walletRepository.findById(transaction.payer())
+        .map(payer -> isTransactionValid(transaction, payer) ? transaction : null)
+        .orElseThrow())
+      .orElseThrow();
+  }
+
+  private boolean isTransactionValid(Transaction transaction, Wallet payer) {
+    return payer.type() == WalletType.COMMON.getValue() && 
+      payer.balance().compareTo(transaction.value()) >= 0 &&
+      !payer.id().equals(transaction.payee());
   }
 }
