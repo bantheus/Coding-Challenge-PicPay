dcce129551f41d44ccf37f384bdb098723011e33
bantheus <matheuschmidt.b@gmail.com>
2024-05-15 17:43:04 -0300

    ✨ feat: add notification custom exception

---
+++ src/main/java/com/bantheus/picpaysimplificado/notification/NotificationException.java
@@ -0,0 +1,7 @@
+package com.bantheus.picpaysimplificado.notification;
+
+public class NotificationException extends RuntimeException {
+  public NotificationException(String message) {
+    super(message);
+  }
+}
