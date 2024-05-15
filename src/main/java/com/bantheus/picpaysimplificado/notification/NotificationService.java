7dbf125f521f8cbaaed3031d9979b9e5128132a1
bantheus <matheuschmidt.b@gmail.com>
2024-05-15 17:42:01 -0300

    ✨ feat: add notification service

---
+++ src/main/java/com/bantheus/picpaysimplificado/notification/NotificationService.java
@@ -0,0 +1,12 @@
+package com.bantheus.picpaysimplificado.notification;
+
+import org.springframework.stereotype.Service;
+
+import com.bantheus.picpaysimplificado.transaction.Transaction;
+
+@Service
+public class NotificationService {
+  public void notify(Transaction transaction) {
+    
+  }
+}
